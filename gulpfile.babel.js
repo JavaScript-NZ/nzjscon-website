a63bc84ca932199b0bc767e01d7ef20028c926f9
Jen Zajac <jen.zajac@catalyst.net.nz>
2016-09-14 15:13:51 +1200

    Add transpilation and polyfills

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import postcss from 'gulp-postcss'
 import cssnano from 'cssnano'
 import autoprefixer from 'autoprefixer'
 import flexbugsfixes from 'postcss-flexbugs-fixes'
+import babel from 'gulp-babel'
 
 // Turn sass into css, prefix, minify and bless
 gulp.task('scss', () => {
@@ -22,34 +23,46 @@ gulp.task('scss', () => {
 
   return gulp.src('src/scss/**/*.scss')
     .pipe(nodesass({
-        includePaths: [
-            'node_modules/reeeset/dist',
-            'node_modules/bootstrap/scss'
-        ]
-    }).on('error', nodesass.logError))
+      includePaths: [
+        'node_modules/reeeset/dist',
+        'node_modules/bootstrap/scss'
+      ]
+    })
+    .on('error', nodesass.logError))
     .pipe(postcss(processors))
     .pipe(gulp.dest('dist/css/'))
 })
 
-// Copy JS files to /dist
-gulp.task('copy-js', () => {
-    return gulp.src(['src/js/**/*'])
+// Transpile ES6 into /dist
+gulp.task('transpile-js', () => {
+  return gulp.src(['src/js/**/*.js'])
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('js-polyfills', function(){
+  return gulp.src([
+      'src/libs/**/*.js',
+      'node_modules/babel-polyfill/dist/polyfill.js'
+    ])
+    .pipe(gulp.dest('dist/libs'))
 })
 
 // Copy image files to /dist
 gulp.task('copy-img', () => {
-    return gulp.src(['src/img/**/*'])
+  return gulp.src(['src/img/**/*'])
     .pipe(gulp.dest('dist/img'))
 })
 
 // Watch files for changes
 gulp.task('watch', () => {
-    gulp.watch('src/**/*', ['scss', 'copy-js', 'copy-img'])
+  gulp.watch('src/**/*', ['scss', 'transpile-js', 'copy-img'])
 })
 
-// Default task (rebuild on init before watching)
-gulp.task('default', ['scss', 'copy-js', 'copy-img', 'watch'])
+// Build
+gulp.task('build', ['scss', 'js-polyfills', 'transpile-js', 'copy-img'])
 
-// Build with no watch
-gulp.task('build', ['scss', 'copy-js', 'copy-img'])
+// Default task (rebuild on init before watching)
+gulp.task('default', ['build', 'watch'])
---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+// note: this ES6 is transpiled to ES5.
+// there is no module loader for require/import won't work.
