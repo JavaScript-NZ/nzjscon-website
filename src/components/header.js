32899a98b3e9e6ccb83cdf15ad1dcfcc62813598
jenofdoom <j_zajac@email.com>
2019-10-19 15:31:03 +1300

    Add bootstrap and react-bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,31 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#F0DB4F`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#323330`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "~/components/header"
+import Container from 'react-bootstrap/Container'
 import "~/components/layout.scss"
 
 const Layout = ({ children }) => {
@@ -24,25 +25,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div className="d-flex flex-column site-wrapper">
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
+
+      <main role="main" className="flex-shrink-0">
+        <Container>
+          {children}
+        </Container>
+      </main>
+
+      <footer className="footer mt-auto py-3">
+        <Container>
           <p>© {new Date().getFullYear()} <a href="mailto:conference@javascript.org.nz">nz.js(con);</a></p>
           <p>Built with <a href="https://www.gatsbyjs.org">Gatsby</a></p>
+          <p className="text-muted"><span className="conf-name">nz.js(con);</span> is run by <a href="http://javascript.org.nz/">JavaScript NZ Incorporated</a>. Want to get involved? Join the <a href="https://javascriptnewzealand.slack.com/messages/nzjscon/">#nzjscon</a> channel on our <a href="http://slack.javascript.org.nz/">public slack channel</a>.</p>
+        </Container>
+      </footer>
 
-          <p><span class="conf-name">nz.js(con);</span> is run by <a href="http://javascript.org.nz/">JavaScript NZ Incorporated</a>. Want to get involved? Join the <a href="https://javascriptnewzealand.slack.com/messages/nzjscon/">#nzjscon</a> channel on our <a href="http://slack.javascript.org.nz/">public slack channel</a>.</p>
-        </footer>
-      </div>
-    </>
+    </div>
   )
 }
 
