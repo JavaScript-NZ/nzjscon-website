a39b1da5d0d32f707297d1e150265529593b0cb3
jenofdoom <j_zajac@email.com>
2019-10-19 16:51:15 +1300

    Set up css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,19 @@ import Layout from "~/components/layout"
 import Image from "~/components/image"
 import SEO from "~/components/seo"
 
+import styles from "~/pages/index.module.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO />
-    <h1>Hi people</h1>
+
+    <div className={styles.hero}>
+      <h1>
+        nz.js(con);<br />
+        wlg 2020
+      </h1>
+    </div>
+
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
